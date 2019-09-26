c10b7de644fd5674f65c82783d9f45f6516a9a90
Spaghetti Jones <connor.breault@comcast.net>
2019-09-26 01:45:25 -0700

    boilerplate added

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,13 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
+
 CREATE TABLE burgers
 (
-    id INT
-    auto_increment NOT NULL,
-    burger_name VARCHAR (50) NOT NULL,
-    devoured BOOLEAN,
+    id INT NOT NULL
+    AUTO_INCREMENT,
+    burger_name VARCHAR
+    (50) NOT NULL,
+    devoured BOOLEAN NOT NULL default 0,
+    PRIMARY KEY
+    (id)
 )
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers_db
+    (burger_name, devoured)
+VALUES
+    ("The Big Chungus", false);
+INSERT INTO burgers_db
+    (burger_name, devoured)
+VALUES
+    ("Beyond Yeet Vegetarian Patty", false);
+INSERT INTO burgers_db
+    (burger_name, devoured)
+VALUES
+    ("Last of the Mo-jicama (comes with jicama)", false);
